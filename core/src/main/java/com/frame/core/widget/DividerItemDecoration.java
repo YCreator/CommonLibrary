2e196abede4b4e76f52b3f38b84c1695ea168bf8
YCreator <yzd.dong@foxmail.com>
2017-09-02 14:24:02 +0800

    www

--- core/src/main/java/com/frame/core/widget/DividerItemDecoration.java
+++ core/src/main/java/com/frame/core/widget/DividerItemDecoration.java
@@ -29,6 +29,8 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
      */
     private int mItemSize = 1;
 
+    private boolean drawFootDividerable = true;
+
     /**
      * 绘制item分割线的画笔，和设置其属性
      * 来绘制个性分割线
@@ -42,6 +44,19 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
     public DividerItemDecoration(Context context, int mOrientation, float dp, @ColorInt int color) {
         this.mOrientation = mOrientation;
+        if (mOrientation != LinearLayoutManager.VERTICAL && mOrientation != LinearLayoutManager.HORIZONTAL) {
+            throw new IllegalArgumentException("请传入正确的参数");
+        }
+        mItemSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp
+                , context.getResources().getDisplayMetrics());
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(color);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    public DividerItemDecoration(Context context, int mOrientation, float dp, @ColorInt int color, boolean drawFootDividerable) {
+        this.mOrientation = mOrientation;
+        this.drawFootDividerable = drawFootDividerable;
         if (mOrientation != LinearLayoutManager.VERTICAL && mOrientation != LinearLayoutManager.HORIZONTAL) {
             throw new IllegalArgumentException("请传入正确的参数");
         }
@@ -68,6 +83,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
     /**
      * 设置item分割线的size
+     *
      * @param outRect
      * @param view
      * @param parent
@@ -76,45 +92,50 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         if (mOrientation == LinearLayoutManager.VERTICAL) {
-            outRect.set(0,0,0,mItemSize);
+            outRect.set(0, 0, 0, mItemSize);
         } else {
-            outRect.set(0,0,mItemSize,0);
+            outRect.set(0, 0, mItemSize, 0);
         }
     }
 
     /**
      * 绘制纵向 item 分割线
+     *
      * @param canvas
      * @param parent
      */
-    private void drawVertical(Canvas canvas,RecyclerView parent){
-        final int left = parent.getPaddingLeft() ;
-        final int right = parent.getMeasuredWidth() - parent.getPaddingRight() ;
-        final int childSize = parent.getChildCount() ;
-        for(int i = 0 ; i < childSize ; i ++){
-            final View child = parent.getChildAt( i ) ;
+    private void drawVertical(Canvas canvas, RecyclerView parent) {
+        final int left = parent.getPaddingLeft();
+        final int right = parent.getMeasuredWidth() - parent.getPaddingRight();
+        final int childSize = parent.getChildCount();
+        for (int i = 0; i < childSize; i++) {
+            if (!drawFootDividerable && i == childSize - 1) {
+                continue;
+            }
+            final View child = parent.getChildAt(i);
             RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) child.getLayoutParams();
-            final int top = child.getBottom() + layoutParams.bottomMargin ;
-            final int bottom = top + mItemSize ;
-            canvas.drawRect(left,top,right,bottom,mPaint);
+            final int top = child.getBottom() + layoutParams.bottomMargin;
+            final int bottom = top + mItemSize;
+            canvas.drawRect(left, top, right, bottom, mPaint);
         }
     }
 
     /**
      * 绘制横向 item 分割线
+     *
      * @param canvas
      * @param parent
      */
-    private void drawHorizontal(Canvas canvas,RecyclerView parent){
-        final int top = parent.getPaddingTop() ;
-        final int bottom = parent.getMeasuredHeight() - parent.getPaddingBottom() ;
-        final int childSize = parent.getChildCount() ;
-        for(int i = 0 ; i < childSize ; i ++){
-            final View child = parent.getChildAt( i ) ;
+    private void drawHorizontal(Canvas canvas, RecyclerView parent) {
+        final int top = parent.getPaddingTop();
+        final int bottom = parent.getMeasuredHeight() - parent.getPaddingBottom();
+        final int childSize = parent.getChildCount();
+        for (int i = 0; i < childSize; i++) {
+            final View child = parent.getChildAt(i);
             RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) child.getLayoutParams();
-            final int left = child.getRight() + layoutParams.rightMargin ;
-            final int right = left + mItemSize ;
-            canvas.drawRect(left,top,right,bottom,mPaint);
+            final int left = child.getRight() + layoutParams.rightMargin;
+            final int right = left + mItemSize;
+            canvas.drawRect(left, top, right, bottom, mPaint);
         }
     }
 }
